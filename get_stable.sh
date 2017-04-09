a4c088fd43601efe806406ffb65768a3595ae59f
Marcel Otte <otte.mm@googlemail.com>
2017-04-09 16:34:53 +0200

    added launcher generation script

--- get_stable.sh
+++ get_stable.sh
@@ -5,3 +5,5 @@ wget http://jenkins.terasology.org/job/DistroOmegaRelease/lastSuccessfulBuild/ar
 wget http://jenkins.terasology.org/job/DistroOmegaRelease/lastSuccessfulBuild/artifact/distros/omega/build/distributions/sha256sums.txt
 
 sha256sum -c sha256sums.txt
+
+unzip TerasologyOmega.zip -d terasology
