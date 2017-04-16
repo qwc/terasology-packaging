621f304a180adc96ab8b548b58704e4c75a1bcbd
Marcel Otte <otte.mm@googlemail.com>
2017-04-16 22:13:02 +0200

    reactivated download of get_launcher.sh

--- get_launcher.sh
+++ get_launcher.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/distributions/TerasologyLauncher.zip
-#wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/distributions/md5sums.txt
-#wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/distributions/sha256sums.txt
+wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/distributions/TerasologyLauncher.zip
+wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/distributions/md5sums.txt || true
+wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/distributions/sha256sums.txt || true
 
-#sha256sum -c sha256sums.txt
+sha256sum -c sha256sums.txt || true
 wget http://jenkins.terasology.org/view/Launcher/job/TerasologyLauncher/lastSuccessfulBuild/artifact/build/resources/main/org/terasology/launcher/version/versionInfo.properties
 
 unzip TerasologyLauncher.zip
